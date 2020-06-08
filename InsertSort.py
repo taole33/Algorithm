216320fd74311602fa057129be35f3f2a7105a80
Shirai <taole33@hotmail.com>
2020-06-08 22:23:21 +0900

    Update InsertSort.py

--- InsertSort.py
+++ InsertSort.py
@@ -9,7 +9,6 @@ for i in range (N):
     get_random = random.randint((-N)*10,N*10)
     n_list.append(get_random)
 
-print(n_list)
 for j in range(N):
     for i in range(j,0,-1):
         if n_list[i-1] > n_list[i]:
